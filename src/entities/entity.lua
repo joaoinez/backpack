c318d7f8a7bac7fb06859e77a76c528e2a233a88
João Inez <joaoinez.info@gmail.com>
2026-03-27 14:12:00 +0100

    feat: add id to entities

--- src/entities/entity.lua
+++ src/entities/entity.lua
@@ -1,8 +1,11 @@
+local uuid = require 'src.utils.uuid'
+
 ---@class (exact) EntityPosition
 ---@field x number
 ---@field y number
 
 ---@class Entity
+---@field protected id string
 ---@field protected type string
 ---@field protected position EntityPosition
 local Entity = {}
@@ -14,11 +17,14 @@ function Entity:new(type, position)
   local o = {}
   setmetatable(o, self)
 
+  o.id = uuid()
   o.type = type
   o.position = position
 
   return o
 end
+
+function Entity:getId() return self.id end
 
 function Entity:getPosition() return self.position end
 
